3a33def55d8190031fc0ee5b0aebb60ba65f0305
Valentina9990 <dania.v.sarmiento@gmail.com>
2024-11-01 15:56:31 -0500

    Refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
-import { ErrorComponent } from './components/error/error.component';
-import { LogoutComponent } from './components/logout/logout.component';
+import { ErrorComponent } from './core/error/error.component';
+import { LogoutComponent } from './core/logout/logout.component';
+import { UsersComponent } from './components/users/users.component';
 
 export const routes: Routes = [
     {
         path: 'logout',
         component: LogoutComponent
+    },
+    {
+        path: 'users',
+        component: UsersComponent
     },
     {
         path: '**',
