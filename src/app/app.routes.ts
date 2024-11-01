fcd7efbb1ef5f4abbded7912556e6f41d7400533
DavuKX <duvandavid2001@gmail.com>
2024-11-01 11:01:34 -0500

    Add error component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ErrorComponent } from './components/error/error.component';
 
 export const routes: Routes = [
     {
         path: 'dashboard',
         component: DashboardComponent
+    },
+    {
+        path: '**',
+        component: ErrorComponent
     }
 ];
