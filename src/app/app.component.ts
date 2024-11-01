96bf8c8b3fc1d1455e2012ef9cf5628e38da74aa
DavuKX <duvandavid2001@gmail.com>
2024-11-01 10:47:55 -0500

    crear layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, NgFor, NgClass, FontAwesomeModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'cinema';
+  title = 'Cine';
+  pages = [
+    { name: 'Dashboard', path: 'dashboard', icon: 'fas fa-home'},
+    { name: 'Peliculas', path: 'movies', icon: 'fas fa-home'},
+    { name: 'Salas', path: 'rooms', icon: 'fas fa-home'},
+    { name: 'Comidas', path: 'foods', icon: 'fas fa-home'},
+    { name: 'Funciones', path: 'functions', icon: 'fas fa-home'},
+  ];
 }
