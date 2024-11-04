49ccdc4c5c59411f33457bcd0a987e4f4d25b2c2
Brayan744 <brayanmunioz6@gmail.com>
2024-11-04 17:53:44 -0500

    Create cine.component.ts

---
+++ src/app/components/cine/cine.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cine',
+  standalone: true,
+  imports: [],
+  templateUrl: './cine.component.html',
+  styleUrl: './cine.component.css'
+})
+export class CineComponent {
+
+}
