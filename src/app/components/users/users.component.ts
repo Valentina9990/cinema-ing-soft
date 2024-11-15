8c0247f8e2baa208df2208a4b8f87371eeb56884
Valentina9990 <dania.v.sarmiento@gmail.com>
2024-11-15 14:09:12 -0500

    CRUD user

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,101 +1,150 @@
-import { Component, OnDestroy, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UserService } from '../../services/api/user.service';
 import { Subscription } from 'rxjs';
-import { User } from '../../core/models/User';
+import { User } from '../../core/models/User.model';
 import { CommonModule } from '@angular/common';
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { UsersResponse } from '../../core/models/UsersResponse.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, NgbPaginationModule],
+  imports: [CommonModule, NgbPaginationModule, FormsModule],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
-export class UsersComponent implements OnInit, OnDestroy{
-  subscriptions:Subscription[];
-  users: User[];
-  newUser: Partial<User>;
+export class UsersComponent implements OnInit, OnDestroy {
+  subscriptions: Subscription[] = [];
+  users: User[] = [];
   page = 1;
-  pageSize = 15;
+  pageSize = 30;
   totalUsers = 0;
-  constructor(
-    private userService: UserService
-  ){
-    this.subscriptions = [];
-    this.users = [];
-    this.newUser = {};
-  }
+  selectedUser: any = {};
+  errorMessage: string = '';
+  successMessage: string = '';
+  newUser: any = {};
+  isUserFormOpen = false;
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
     this.getUsers();
   }
 
   ngOnDestroy(): void {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 
-  public getUsers(): void {
-    const usersSub = this.userService.getUsers().subscribe({
-      next: (response: User[]) => {
-        this.users = response;
-        this.totalUsers = response.length;
-        console.log('Users fetched successfully:', response);
-      },
-      error: (error) => {
-        console.error('Error fetching users:', error);
-      }
-    });
+  getUsers(): void {
+    const offset = (this.page - 1) * this.pageSize;
+    const usersSub = this.userService
+      .getUsers(this.pageSize, offset)
+      .subscribe({
+        next: (response: UsersResponse) => {
+          this.users = response.rows;
+          this.totalUsers = response.total;
+          console.log('Users fetched successfully:', this.users);
+          console.log('Total users:', this.totalUsers);
+        },
+        error: (error) => {
+          console.error('Error fetching users:', error);
+        },
+      });
     this.subscriptions.push(usersSub);
-  }
-
-  addUser(): void {
-    this.userService.addUser(this.newUser as Omit<User, 'idUsuario' | 'fechaCreacion'>).subscribe({
-      next: (createdUser) => {
-        this.users.push(createdUser);
-        console.log('User added successfully:', createdUser);
-      },
-      error: (error) => {
-        console.error('Error adding user:', error);
-      }
-    });
-  }
-
-  updateUser(userId: number): void {
-    const updatedUser = this.newUser as Omit<User, 'idUsuario'>;
-    this.userService.updateUserById(userId, updatedUser).subscribe({
-      next: (user) => {
-        const index = this.users.findIndex(u => u.idUsuario === user.idUsuario);
-        if (index !== -1) {
-          this.users[index] = user;
-        }
-        console.log('User updated successfully:', user);
-      },
-      error: (error) => {
-        console.error('Error updating user:', error);
-      }
-    });
-  }
-
-  deleteUser(userId: number): void {
-    this.userService.deleteUserById(userId).subscribe({
-      next: () => {
-        this.users = this.users.filter(u => u.idUsuario !== userId);
-        console.log(`User with ID ${userId} deleted successfully`);
-      },
-      error: (error) => {
-        console.error('Error deleting user:', error);
-      }
-    });
-  }
-
-  get paginatedUsers(): User[] {
-    const startItem = (this.page - 1) * this.pageSize;
-    const endItem = this.page * this.pageSize;
-    return this.users.slice(startItem, endItem);
   }
 
   onPageChange(page: number): void {
     this.page = page;
+    this.getUsers();
   }
+
+  addUser(): void {
+    this.userService.addUser(this.newUser).subscribe({
+      next: (response) => {
+        this.successMessage = '¡Usuario agregado con éxito!';
+        setTimeout(() => {
+          this.successMessage = '';
+        }, 2000);
+        this.getUsers();
+        this.closeUserForm();
+      },
+      error: (error) => {
+        this.errorMessage = error.error.respuesta || 'Error al crear el usuario';
+        setTimeout(() => {
+          this.errorMessage = '';
+        }, 2000);
+      },
+    });
+  }
+
+  openUserForm(): void {
+    this.isUserFormOpen = true;
+    this.newUser = {};
+  }
+
+  closeUserForm(): void {
+    this.isUserFormOpen = false;
+  }
+
+  editUser(user: any): void {
+    this.selectedUser = { ...user };
+  }
+
+  deleteUser(idUsuario: number): void {
+    if (confirm('¿Está seguro que desea eliminar este usuario?')) {
+      this.userService.deleteUser(idUsuario).subscribe({
+        next: () => {
+          this.getUsers();
+          this.errorMessage = '';
+          this.successMessage = '¡Usuario eliminado con éxito!';
+          setTimeout(() => {
+            this.successMessage = '';
+          }, 2000);
+        },
+        error: (error) => {
+          console.error('Error deleting user:', error);
+          this.errorMessage =
+            error.error.respuesta || 'Error al actualizar el usuario';
+          setTimeout(() => {
+            this.errorMessage = '';
+          }, 2000);
+        },
+      });
+    }
+  }
+
+  saveChanges(user: any): void {
+    const userData = {
+      id_usuario: user.idUsuario,
+      nombre_usuario: user.nombreUsuario,
+      apellido_usuario: user.apellidoUsuario,
+      contrasena_usuario: user.contrasenaUsuario,
+      email_usuario: user.emailUsuario,
+      fecha_creacion: user.fechaCreacion,
+      fecha_nacimiento_usuario: user.fechaNacimientoUsuario,
+      id_cine: user.idCine,
+      id_cargo: user.idCargo,
+    };
+
+    this.userService.updateUser(userData).subscribe({
+      next: () => {
+        this.getUsers();
+        this.selectedUser = {};
+        this.errorMessage = '';
+        this.successMessage = '¡Usuario modificado con éxito!';
+          setTimeout(() => {
+            this.successMessage = '';
+          }, 2000);
+      },
+      error: (error) => {
+        console.error('Error updating user:', error);
+        this.errorMessage =
+          error.error.respuesta || 'Error al actualizar el usuario';
+          setTimeout(() => {
+            this.errorMessage = '';
+          }, 2000);
+      },
+    });
+  }
+  
 }
